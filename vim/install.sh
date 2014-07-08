7758e343467a10a68888b3f2521e194194851497
Benny Chen <youfychenbc5000@gmail.com>
2014-07-08 16:46:08 -0700

    minor fixes

--- vim/install.sh
+++ vim/install.sh
@@ -1,17 +1,14 @@
 cp .jshintrc ~/
 cp .vimrc ~/
-sudo apt-get install git build-essential curl
+sudo apt-get install git build-essential curl wget cmake
 sudo npm install -g jshint
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+wget -O ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd ~/.vim/bundle/
 git clone https://github.com/jelera/vim-javascript-syntax.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/Valloric/YouCompleteMe.git
 git clone https://github.com/Lokaltog/vim-distinguished
 cd YouCompleteMe
-cd third_party
 git submodule update --init --recursive
-cd ..
 ./install.sh --clang-completer
